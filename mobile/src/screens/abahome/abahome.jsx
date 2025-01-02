52e68f02f9726573eeb14d41d6c763665a97734e
vandodev <vandoaparecido@hotmail.com>
2025-01-02 08:36:10 -0300

    :ambulance: :heart: :hospital: Mobile - Buscando dados fictícios para formatar  a FletList

--- mobile/src/screens/abahome/abahome.jsx
+++ mobile/src/screens/abahome/abahome.jsx
@@ -1,9 +1,19 @@
-import {Text, View} from 'react-native';
+import {FlatList, Text, View} from 'react-native';
 import {styles} from "./abahome.style.js";
+import { doctors } from '../../constants/data.js';
 
 function AbaHome() {
     return <View style={styles.container}>
         <Text style={styles.text}>Agende os seus serviços médicos!</Text>
+
+        <FlatList data={doctors} 
+            keyExtractor={(doc)=> doc.id_doctor}
+            showsVerticalScrollIndicator={false}
+            renderItem={({ item }) => {
+                return <Text>{item.name}</Text>    
+            }}
+         />
+
     </View>
 }
 
