e27b51538ad86d15b4cce3b01df55dcf1126e4b6
vandodev <vandoaparecido@hotmail.com>
2025-01-04 10:05:46 -0300

    :ambulance: :heart: :hospital: Mobile - Trabalhando com react-native-picker

--- mobile/src/screens/schedule/schedule.jsx
+++ mobile/src/screens/schedule/schedule.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Text, View } from "react-native";
+import { Picker } from "@react-native-picker/picker";
 import { Calendar, LocaleConfig } from "react-native-calendars";
 import { styles } from "./schedule.style.js";
 import { ptBR } from "../../constants/calendar";
@@ -8,9 +9,10 @@ import Button from "../../components/button/button.jsx";
 LocaleConfig.locales["pt-br"] = ptBR;
 LocaleConfig.defaultLocale = 'pt-br';
 
-function Schedule(props) {
-  const [data, setData] = useState();
-
+function Schedule() {
+  const [data, setData] = useState(new Date().toISOString().slice(0,10));
+  const [horario, setHorario] = useState("9:00");
+   
   return (
     <View  style={styles.container}>
       <Calendar 
@@ -23,6 +25,23 @@ function Schedule(props) {
       />
 
       <Text style={styles.horario}>Horário</Text>
+
+      <Picker 
+        selectedValue={horario} 
+        onValueChange={(itemValue, itemIndex) => {
+          setHorario(itemValue);
+      }}>
+        <Picker.Item label="09:00" value="09:00" />
+        <Picker.Item label="09:30" value="09:30" />
+        <Picker.Item label="10:00" value="10:00" />
+        <Picker.Item label="11:00" value="11:00" />
+        <Picker.Item label="12:00" value="12:00" />
+        <Picker.Item label="13:00" value="13:00" />
+        <Picker.Item label="14:00" value="14:00" />
+        <Picker.Item label="15:00" value="15:00" />
+        <Picker.Item label="16:00" value="16:00" />
+        <Picker.Item label="17:00" value="17:00" />
+      </Picker>
   
       <View style={styles.btnContainer}>
         <Button text="Confirmar Agendamento" />
