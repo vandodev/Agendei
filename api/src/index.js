e575adccece69b9cc4e613a55914438ea44273e9
vandodev <vandoaparecido@hotmail.com>
2025-01-05 07:33:12 -0300

    :ambulance: :heart: :hospital: Server - Criando o projeto e rodando o servidor

---
+++ api/src/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json())
+app.use(cors());
+
+
+app.listen(3001, () => {
+    console.log(`Servidor Agendei rodando na porta 3001`);
+})
