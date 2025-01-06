86943a97c4b21eaa5bb59087bca0d498b3d2b167
vandodev <vandoaparecido@hotmail.com>
2025-01-06 08:21:02 -0300

    :ambulance: :heart: :hospital: Server - Inserção com seed

---
+++ api/src/database/seed.sql
@@ -0,0 +1,8 @@
+INSERT INTO doctors (id_doctor, name, specialty, icon) 
+VALUES (1, "Fernando", "Cardiologista", "M");
+
+INSERT INTO doctors (id_doctor, name, specialty, icon) 
+VALUES (2, "Heber", "Clínico Geral", "M");
+
+INSERT INTO doctors (id_doctor, name, specialty, icon) 
+VALUES (3, "Patrícia", "Dermatologista", "F");
