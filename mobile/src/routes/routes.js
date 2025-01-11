f8b5d3c97c0c50e961ba2777135883c8c5a9ddcb
vandodev <vandoaparecido@hotmail.com>
2025-01-11 07:21:59 -0300

    :ambulance: :heart: :hospital: Mobile - Configurando axios

--- mobile/src/routes/routes.js
+++ mobile/src/routes/routes.js
@@ -3,7 +3,7 @@ import RoutesOpen from "./routesOpen"
 
 function Routes(){
     const user = {
-        id_user:1
+        // id_user:1
     }
 
     return user.id_user ? <RoutesPrivate/> : <RoutesOpen/>
