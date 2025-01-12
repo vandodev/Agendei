3d00bc17c11d0054e7e64e865ffda8e01d43b511
vandodev <vandoaparecido@hotmail.com>
2025-01-12 08:21:10 -0300

    :ambulance: :heart: :hospital: Mobile - Buscando dados médicos da api

--- mobile/src/screens/abahome/abahome.jsx
+++ mobile/src/screens/abahome/abahome.jsx
@@ -1,15 +1,36 @@
-import {FlatList, Text, View} from 'react-native';
+import { useEffect, useState } from "react";
+import {FlatList, Alert, Text, View} from 'react-native';
 import {styles} from "./abahome.style.js";
-import { doctors } from '../../constants/data.js';
 import Doctor from '../../components/doctor/doctor.jsx';
+import api from "../../constants/api.js";
 
 function AbaHome(props) {
+
+    const [doctors, setDoctors] = useState([]);
 
     function handleClick(id_doctor, name, specialty, icon) {
         props.navigation.navigate("services",{
             id_doctor, name, specialty, icon: icon
         });
     }
+
+    async function loadDoctors() {
+        try {
+          const response = await api.get("doctors");
+          setDoctors(response.data);
+        }
+        catch (error) {
+          if (error.response?.data.error)
+            Alert.alert(error.response.data.error);
+          else
+            Alert.alert("Ocorreu um erro, tente novamente mais tarde");
+          console.log(error);
+        }
+      }
+    
+      useEffect(() => {
+        loadDoctors();
+      }, []);
 
     return <View style={styles.container}>
         <Text style={styles.text}>Agende os seus serviços médicos!</Text>
