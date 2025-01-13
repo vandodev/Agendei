8d0a7649762f90891e88fe9863d08317f2e5cbeb
vandodev <vandoaparecido@hotmail.com>
2025-01-13 08:35:36 -0300

    :ambulance: :heart: :hospital: Mobile - Exibindo informações do perfil de usuário

--- mobile/src/screens/abaprofile/abaprofile.jsx
+++ mobile/src/screens/abaprofile/abaprofile.jsx
@@ -1,18 +1,44 @@
+import { useEffect, useState } from "react";
 import { Alert, Text, View } from "react-native"
 import { styles } from "./abaprofile.style";
 import Button from "../../components/button/button.jsx";
+import api from "../../constants/api";
 
 function AbaProfile() {
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+
+  async function loadProfile() {
+    try {
+      const response = await api.get("users/profile");
+
+      if (response.data) {
+        setNome(response.data?.name);
+        setEmail(response.data?.email);
+      }
+    }
+    catch (error) {
+      if (error.response?.data.error)
+        Alert.alert(error.response.data.error);
+      else
+        Alert.alert("Ocorreu um erro, tente novamente mais tarde");
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    loadProfile();
+  }, []);
  
   return  <View style={styles.container}>
       <View style={styles.item}>
         <Text style={styles.title}>Nome</Text>
-        <Text style={styles.text}>Evandro Aparecido de Oliveira</Text>  
+        <Text style={styles.text}>{nome}</Text>  
       </View>
       
       <View style={styles.item}>
         <Text style={styles.title}>E-mail</Text>
-        <Text style={styles.text}>vandoaparecido@hotmail.com</Text>
+        <Text style={styles.text}>{email}</Text>
       </View>
 
       <View style={styles.item}>
