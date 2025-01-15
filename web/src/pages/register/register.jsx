221fde0add05a7bdc39016be755b7927d3d6bab7
vandodev <vandoaparecido@hotmail.com>
2025-01-15 09:11:47 -0300

    :ambulance: :heart: :hospital: Web - Criando função de cadastro

--- web/src/pages/register/register.jsx
+++ web/src/pages/register/register.jsx
@@ -1,9 +1,49 @@
+import { useState } from "react";
 import "./register.css";
 import logo from "../../assets/logo.png";
 import fundo from "../../assets/fundo.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import api from "../../constants/api";
 
 function Register() {
+
+    const navigate = useNavigate();
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [password2, setPassword2] = useState("");
+    const [msg, setMsg] = useState("");
+
+
+    async function ExecuteAccount() {
+        setMsg("");
+        if(password != password2) {
+            return setMsg("As senhas não conferem. Digite novamete.")
+        }
+        try {
+            const response = await api.post("/users/register", {
+                name,
+                email,
+                password
+            });
+            if (response.data) {
+                localStorage.setItem("sessionToken", response.data.token);
+                localStorage.setItem("sessionId", response.data.id_user);
+                localStorage.setItem("sessionEmail", email);
+                localStorage.setItem("sessionName", name);
+                navigate("/appointments");
+            } else {
+                setMsg("Erro ao fazer login, tente novamente mais tarde!");
+            }
+        } catch (error) {
+            if (error.response?.data.error) {
+                setMsg(error.response?.data.error);
+            } else {
+                setMsg("Erro ao fazer login, tente novamente mais tarde!");
+            }
+        }
+    }
+
     return (
         <div className="row">
             <div className="col-sm-5 d-flex justify-content-center align-items-center text-center">
@@ -13,20 +53,52 @@ function Register() {
                     <h5 className="mb-4 text-secondary">Preencha os campos abaixo</h5>
                     
                     <div className="mt-4">
-                        <input type="text" placeholder="Nome" className="form-control" />
+                        <input
+                            type="text"
+                            placeholder="Nome"
+                            className="form-control"
+                            onChange={(e) => setName(e.target.value)}
+                        />
                     </div>
+
                     <div className="mt-2">
-                        <input type="email" placeholder="E-mail" className="form-control" />
+                        <input
+                            type="email"
+                            placeholder="E-mail"
+                            className="form-control"
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
                     </div>
+
                     <div className="mt-2">
-                        <input type="password" placeholder="Senha" className="form-control" />
+                        <input
+                            type="password"
+                            placeholder="Senha"
+                            className="form-control"
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
                     </div>
+
                     <div className="mt-2">
-                        <input type="password" placeholder="Confirme a senha" className="form-control" />
+                        <input
+                            type="password"
+                            placeholder="Confirme a senha"
+                            className="form-control"
+                            onChange={(e) => setPassword2(e.target.value)}
+                        />
                     </div>
+
                     <div className="mt-3 mb-5">
-                        <button className="btn btn-primary w-100">Criar minha conta</button>
+                        <button onClick={ExecuteAccount} className="btn btn-primary w-100" type="button">Criar minha conta</button>
                     </div>
+
+                    {
+                        msg.length > 0 &&
+                        <div className="alert alert-danger" role="alert">
+                            {msg}
+                        </div>
+                    }
+
                     <div>
                         <span className="me-1">Já tenho uma conta.</span>
                         <Link to="/">Acessar agora!</Link>
